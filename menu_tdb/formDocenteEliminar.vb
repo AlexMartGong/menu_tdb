81df2e45a220e08717eb089a2c797703810a0c55
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2024-03-12 11:13:21 -0600

    Agrego la baja logica

--- menu_tdb/formDocenteEliminar.vb
+++ menu_tdb/formDocenteEliminar.vb
@@ -1,5 +1,43 @@
-﻿Public Class formDocenteEliminar
+﻿Imports MySql.Data.MySqlClient
+
+Public Class formDocenteEliminar
+
+    Dim adCon As MySqlConnection
+    Private cm As MySqlCommand
+
+    Private Sub formDocenteEliminar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            adCon = New MySqlConnection(cadena)
+            adCon.Open()
+        Catch ex As Exception
+            MsgBox(ex.Message, "Error al conectar la base de datos")
+        Finally
+            adCon.Close()
+        End Try
+    End Sub
+
     Private Sub btnCerrar_Click(sender As Object, e As EventArgs) Handles btnCerrar.Click
         Me.Close()
     End Sub
+
+    Private Sub btnBaja_Click(sender As Object, e As EventArgs) Handles btnBaja.Click
+        Try
+            adCon.Open()
+            cm = New MySqlCommand
+            cm.Connection = adCon
+            cm.CommandType = CommandType.Text
+            cm.CommandText = "call spCambiarEstatusDocente(?p_id_docente, ?p_estatus)"
+            cm.Parameters.AddWithValue("p_id_docente", CInt(txtFolioDocente.Text))
+            cm.Parameters.AddWithValue("p_estatus", cbsBaja.Text)
+            cm.ExecuteNonQuery()
+            MsgBox("Datos Guardados")
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            adCon.Close()
+        End Try
+    End Sub
+
+
 End Class
